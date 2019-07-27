45cdb165995e1c70c6dc9d6ae5a6d6981f317c89
邓文浩 <aboutmydreams@163.com>
2019-07-27 22:56:26 +0800

    fix a ui bug

--- lib/ui/common/separator.dart
+++ lib/ui/common/separator.dart
@@ -5,9 +5,8 @@ class Separator extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Container(
         margin: new EdgeInsets.symmetric(vertical: 8.0),
-        height: 2.0,
+        height: 1.0,
         width: 18.0,
-        color: new Color(0xff00c6ff)
-    );
+        color: new Color(0xff00c6ff));
   }
 }
